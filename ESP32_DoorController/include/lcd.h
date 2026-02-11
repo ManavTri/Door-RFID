2483015129d7368fab8e9aa0aa31cb03fa225a3a
ManavTri <manavtrivedy@gmail.com>
2026-02-11 00:30:28 -0500

    Added the lcd logic from the old code

--- ESP32_DoorController/include/lcd.h
+++ ESP32_DoorController/include/lcd.h
@@ -1,10 +1,16 @@
 #pragma once
+#include <Arduino.h>
+#include <LiquidCrystal_I2C.h>
 class LCDDisplay
 {
 private:
-    /* data */
+    LiquidCrystal_I2C lcd;
+    uint8_t cols;
+    uint8_t rows;
+    String clear_row;
 public:
-    LCDDisplay(/* args */);
-    void displayCode(int code);
-    void displayStatus(const char* msg);
+    LCDDisplay(uint8_t address, uint8_t cols, uint8_t rows);
+    void begin();
+    void displayCode(const String& msg);
+    void displayStatus(const String& msg);
 };
