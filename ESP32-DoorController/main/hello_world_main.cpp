d996e97d6d8633328a176165b7dae432f88cbb01
ManavTri <manavtrivedy@gmail.com>
2026-03-05 21:11:06 -0500

    Recreated simple greeting bot (used esp-discord echo example as template)

--- ESP32-DoorController/main/hello_world_main.cpp
+++ ESP32-DoorController/main/hello_world_main.cpp
@@ -6,8 +6,9 @@
 
 #include <stdio.h>
 #include <inttypes.h>
-#include "sdkconfig.h"
+#include <estr.h>
 extern "C" {
+    #include "sdkconfig.h"
     #include "freertos/FreeRTOS.h"
     #include "freertos/task.h"
     #include "esp_chip_info.h"
@@ -18,7 +19,41 @@ extern "C" {
     #include "nvs_flash.h"
     #include "protocol_examples_common.h"
     #include "discord.h"
+    #include "discord/session.h"
+    #include "discord/message.h"
 }
+
+static discord_handle_t bot;
+
+extern "C" static void bot_event_handler(void* handler_arg, esp_event_base_t base, int32_t event_id, void* event_data) {
+    discord_event_data_t* data = (discord_event_data_t*) event_data;
+
+    switch(event_id) {
+        case DISCORD_EVENT_CONNECTED:
+            discord_session_t* session = (discord_session_t*) data->ptr;
+            ESP_LOGI("BOT", "Bot %s#%s connected", session->user->username, session->user->discriminator);
+            break;
+        case DISCORD_EVENT_MESSAGE_RECEIVED:
+            discord_message_t* msg = (discord_message_t*) data->ptr;
+            ESP_LOGI("BOT", "New message (dm=%s, bot=%s, channel=%s, guild=%s, content=%s)", !msg->guild_id ? "true" : "false", msg->author->username, msg->author->discriminator, msg->author->bot ? "true" : "false", msg->channel_id, msg->guild_id ? msg->guild_id : "NULL", msg->content);
+            char* echo_content = estr_cat("Hello ", msg->author->username, ", I am a bot!");
+            discord_message_t echo = {
+                .content = echo_content,
+                .channel_id = msg->channel_id
+            };
+            if (discord_message_send(bot, &echo, NULL) == ESP_OK)
+                ESP_LOGI("BOT", "Message sent successfully");
+            else
+                ESP_LOGE("BOT", "Message failed to send");
+            break;
+        case DISCORD_EVENT_DISCONNECTED:
+            ESP_LOGW("BOT", "Bot disconnected.");
+            break;
+        default:
+            ESP_LOGW("BOT", "Unknown Discord Event");
+    }
+}
+
 extern "C" void app_main(void)
 {
     ESP_ERROR_CHECK(nvs_flash_init());
@@ -26,13 +61,11 @@ extern "C" void app_main(void)
     ESP_ERROR_CHECK(esp_event_loop_create_default());
     ESP_ERROR_CHECK(example_connect());
 
-    ESP_LOGI("BOT", "Ran main");
+    discord_config_t config = {
+        .intents = DISCORD_INTENT_GUILD_MESSAGES
+    };
 
-    for (int i = 10; i >= 0; i--) {
-        printf("Restarting in %d seconds...\n", i);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-    }
-    printf("Restarting now.\n");
-    fflush(stdout);
-    esp_restart();
+    bot = discord_create(&config);
+    discord_register_events(bot, DISCORD_EVENT_ANY, bot_event_handler, NULL);
+    discord_login(bot);
 }
